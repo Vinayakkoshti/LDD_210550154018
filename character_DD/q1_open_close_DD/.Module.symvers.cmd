c3ffbb0e3548e8610efbb2d4c4a4e948849cacfc
vinayak koshti <vinyakkoshti111@gmail.com>
2021-08-14 16:03:50 +0100

    chracter DD open close file

---
+++ character_DD/q1_open_close_DD/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/Desktop/LD/LDD_210550154018/character_DD/q1_open_close_DD/Module.symvers := sed 's/ko$$/o/' /home/pi/Desktop/LD/LDD_210550154018/character_DD/q1_open_close_DD/modules.order | scripts/mod/modpost -m -a   -o /home/pi/Desktop/LD/LDD_210550154018/character_DD/q1_open_close_DD/Module.symvers -e -i Module.symvers   -T -
---
+++ character_DD/q1_open_close_DD/char_DD_open_close.mod
@@ -0,0 +1,2 @@
+/home/pi/Desktop/LD/LDD_210550154018/character_DD/q1_open_close_DD/char_DD_open_close.o
+
